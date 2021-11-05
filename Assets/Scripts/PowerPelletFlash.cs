0e11b0a9d9f704ca5b49efdb0fee1ce907e156e7
Kieran Taverner <kierantav@gmail.com>
2021-11-05 20:47:29 +1100

    Flashing pellets

---
+++ Assets/Scripts/PowerPelletFlash.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPelletFlash : MonoBehaviour
+{
+    public SpriteRenderer powerPelletRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        StartCoroutine(Flash());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Flash() {
+        yield return new WaitForSeconds(3);
+        while (true) {
+            powerPelletRend.enabled = false;
+            yield return new WaitForSeconds(.25f);
+
+            powerPelletRend.enabled = true;
+            yield return new WaitForSeconds(.25f);
+        }
+    }
+}
